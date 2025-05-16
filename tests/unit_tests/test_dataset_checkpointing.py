NousResearch/torchtitan

commit 0b0931cca61964d3541b368995a4707a23518df6

Avoid using skip() in hf_datasets (#838)
Fix issue https://github.com/pytorch/torchtitan/issues/809

The current `self._data.skip(self._sample_idx)` could not get the
correct data for c_4 dataset.
Thus we switch to next() first before the fix is landed.

Test plan:
We reproduce the https://github.com/pytorch/torchtitan/issues/809 by
resuming from checkpoint at step 500, then compare the loss curve in 3
conditions:
1. the origin curve running from step 0 to 750
2. the resumed curve keeping .skip()
3. the resumed curve switch to next(), with this PR change
<img width="354" alt="Screenshot 2025-02-12 at 11 19 24 AM"
src="https://github.com/user-attachments/assets/0ad38637-a071-4f8c-9c4c-5cfd8699ad93"
/>


Warning
for c_4 dataset, if we resume from a large enough step, we call next()
for `self._sample_idx` times, resuming from checkpoint would be much
slower than using .skip()

Next step:
add unit test:
1. test the state_dict check between dcp.save/load and torch.save/load
2. test the difference between next() and .skip()

torchtitan/datasets/hf_datasets.py
@@ -9,15 +9,15 @@ from dataclasses import dataclass
 from typing import Any, Callable, Dict, List, Optional
 
 import torch
+
+from datasets import Dataset, load_dataset
+from datasets.distributed import split_dataset_by_node
 from torch.distributed.checkpoint.stateful import Stateful
 from torch.utils.data import IterableDataset
 from torchdata.stateful_dataloader import StatefulDataLoader
 
 from torchtitan.datasets.tokenizer import Tokenizer
 from torchtitan.logging import logger
-
-from datasets import Dataset, load_dataset
-from datasets.distributed import split_dataset_by_node
 
 
 def _load_c4_dataset(dataset_path: str):
@@ -99,13 +99,13 @@ class HuggingFaceDataset(IterableDataset, Stateful):
         self._all_tokens: List[int] = []
 
     def _get_data_iter(self):
-        if self._sample_idx == 0:
-            return iter(self._data)
-
         if isinstance(self._data, Dataset) and self._sample_idx == len(self._data):
             return iter([])
 
-        return iter(self._data.skip(self._sample_idx))
+        it = iter(self._data)
+        for _ in range(self._sample_idx):
+            next(it)
+        return it
 
     def __iter__(self):
         max_buffer_token_len = 1 + self.seq_len

commit 08ee0282fdfc0d1016c9eb3f5e8240a5a8e3fe7a

Fast dataset resume (#1082)
This PR makes resuming dataset iteration from a checkpoint fast again.

This performance regression comes from
https://github.com/pytorch/torchtitan/pull/838. In that PR, `.skip` is
removed for both map-style and iterable-style datasets for correctness
reasons. However, `.skip` works as expected for map-style datasets, so
the change can be reverted for that case. On the other hand, for
iterable-style datasets, calling `.skip` after `split_dataset_by_node`
splits the number of elements to skip **across the ranks** (e.g. calling
`.skip(10)` after `split_dataset_by_node(<rank>, 2)` effectively skips 5
(`10 // 2 = 5`) elements on each rank), which isn'r what we want/expect,
so removing `.skip` was justified there. Still, we can make the whole
thing much faster using the [`state_dict`
API](https://huggingface.co/docs/datasets/v3.5.0/en/stream#save-a-dataset-checkpoint-and-resume-iteration)
for iterable-style datasets, which avoids re-iterating past shards/files
when resuming.

torchtitan/datasets/hf_datasets.py
@@ -96,16 +96,18 @@ class HuggingFaceDataset(IterableDataset, Stateful):
 
         # Variables for checkpointing
         self._sample_idx = 0
-        self._all_tokens: list[int] = []
+        self._token_buffer: list[int] = []
 
     def _get_data_iter(self):
-        if isinstance(self._data, Dataset) and self._sample_idx == len(self._data):
-            return iter([])
+        # For map-style datasets, resume by skipping to the correct index
+        # For iterable-style datasets, the underlying iterator already points to the correct index
+        if isinstance(self._data, Dataset):
+            if self._sample_idx == len(self._data):
+                return iter([])
+            else:
+                return iter(self._data.skip(self._sample_idx))
 
-        it = iter(self._data)
-        for _ in range(self._sample_idx):
-            next(it)
-        return it
+        return iter(self._data)
 
     def __iter__(self):
         max_buffer_token_len = 1 + self.seq_len
@@ -115,13 +117,13 @@ class HuggingFaceDataset(IterableDataset, Stateful):
                 # Use the dataset-specific text processor
                 sample_text = self._text_processor(sample)
                 sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
-                self._all_tokens.extend(sample_tokens)
+                self._token_buffer.extend(sample_tokens)
                 self._sample_idx += 1
 
-                while len(self._all_tokens) >= max_buffer_token_len:
-                    x = torch.LongTensor(self._all_tokens[:max_buffer_token_len])
+                while len(self._token_buffer) >= max_buffer_token_len:
+                    x = torch.LongTensor(self._token_buffer[:max_buffer_token_len])
                     # update tokens to the remaining tokens
-                    self._all_tokens = self._all_tokens[max_buffer_token_len:]
+                    self._token_buffer = self._token_buffer[max_buffer_token_len:]
                     input = x[:-1]
                     label = x[1:]
                     yield {"input": input}, label
@@ -133,13 +135,33 @@ class HuggingFaceDataset(IterableDataset, Stateful):
                 # Reset offset for the next iteration
                 self._sample_idx = 0
                 logger.warning(f"Dataset {self.dataset_name} is being re-looped")
+                # Ensures re-looping a dataset loaded from a checkpoint works correctly
+                if not isinstance(self._data, Dataset):
+                    if hasattr(self._data, "set_epoch") and hasattr(
+                        self._data, "epoch"
+                    ):
+                        self._data.set_epoch(self._data.epoch + 1)
 
     def load_state_dict(self, state_dict):
-        self._sample_idx = state_dict["sample_idx"]
-        self._all_tokens = state_dict["token_buffer"]
+        self._token_buffer = state_dict["token_buffer"]
+
+        if isinstance(self._data, Dataset):
+            self._sample_idx = state_dict["sample_idx"]
+        else:
+            assert "data" in state_dict
+            self._data.load_state_dict(state_dict["data"])
 
     def state_dict(self):
-        return {"token_buffer": self._all_tokens, "sample_idx": self._sample_idx}
+        _state_dict = {"token_buffer": self._token_buffer}
+
+        if isinstance(self._data, Dataset):
+            _state_dict["sample_idx"] = self._sample_idx
+        else:
+            # Save the iterable dataset's state to later efficiently resume from it
+            # https://huggingface.co/docs/datasets/v3.5.0/en/stream#save-a-dataset-checkpoint-and-resume-iteration
+            _state_dict["data"] = self._data.state_dict()
+
+        return _state_dict
 
 
 def build_hf_dataloader(

tests/unit_tests/test_dataset_checkpointing.py
@@ -4,35 +4,58 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import unittest
+
 import torch
+from datasets import load_dataset
 from torchtitan.config_manager import ConfigManager
-from torchtitan.datasets.hf_datasets import build_hf_dataloader
+from torchtitan.datasets.hf_datasets import build_hf_dataloader, DatasetConfig, DATASETS
 from torchtitan.datasets.tokenizer.tiktoken import TikTokenizer
 
 
-class TestDatasetCheckpointing:
+class TestDatasetCheckpointing(unittest.TestCase):
+    def setUp(self):
+        DATASETS["c4_test_streaming"] = DatasetConfig(
+            path="tests/assets/c4_test",
+            loader=lambda path: load_dataset(path, split="train").to_iterable_dataset(
+                num_shards=4
+            ),
+            text_processor=lambda sample: sample["text"],
+        )
+
+    def tearDown(self):
+        del DATASETS["c4_test_streaming"]
+
     def test_c4_resumption(self):
-        dataset_name = "c4_test"
-        batch_size = 1
-        seq_len = 1024
-        world_size = 4
-        rank = 0
+        for dataset_name in ["c4_test", "c4_test_streaming"]:
+            for world_size in [2, 4]:
+                for rank in range(world_size):
+                    batch_size = 1
+                    seq_len = 1024
 
-        dl = self._build_dataloader(dataset_name, batch_size, seq_len, world_size, rank)
+                    dl = self._build_dataloader(
+                        dataset_name, batch_size, seq_len, world_size, rank
+                    )
 
-        it = iter(dl)
-        for _ in range(250):
-            next(it)
-        state = dl.state_dict()
-        expected_input_ids, expected_labels = next(it)
+                    it = iter(dl)
+                    for _ in range(250):
+                        next(it)
+                    state = dl.state_dict()
 
-        # Create new dataloader, restore checkpoint, and check if next data yielded is the same as above
-        dl = self._build_dataloader(dataset_name, batch_size, seq_len, world_size, rank)
-        dl.load_state_dict(state)
-        input_ids, labels = next(iter(dl))
+                    # Create new dataloader, restore checkpoint, and check if next data yielded is the same as above
+                    dl_resumed = self._build_dataloader(
+                        dataset_name, batch_size, seq_len, world_size, rank
+                    )
+                    dl_resumed.load_state_dict(state)
+                    it_resumed = iter(dl_resumed)
 
-        assert torch.equal(input_ids["input"], expected_input_ids["input"])
-        assert torch.equal(labels, expected_labels)
+                    for _ in range(500):
+                        expected_input_ids, expected_labels = next(it)
+                        input_ids, labels = next(it_resumed)
+                        assert torch.equal(
+                            input_ids["input"], expected_input_ids["input"]
+                        )
+                        assert torch.equal(labels, expected_labels)
 
     def _build_dataloader(self, dataset_name, batch_size, seq_len, world_size, rank):
         tokenizer = TikTokenizer("./tests/assets/test_tiktoken.model")