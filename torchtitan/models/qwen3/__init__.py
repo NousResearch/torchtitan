cf75fb7f80d299f2d1547f67cdb792577561c3a1
Dakota Mahan <dmayhem93@gmail.com>
2025-07-17 11:21:01 -0500

    add qwen3 modeling code

--- torchtitan/models/qwen3/__init__.py
+++ torchtitan/models/qwen3/__init__.py
@@ -14,101 +14,85 @@ from torchtitan.components.validate import build_validator
 from torchtitan.datasets.dataloader import build_dataloader
 from torchtitan.protocols.train_spec import register_train_spec, TrainSpec
 
-from .infra.parallelize import parallelize_llama
-from .infra.pipeline import pipeline_llama
+from .infra.parallelize import parallelize_qwen3
+from .infra.pipeline import pipeline_qwen3
 from .model.args import TransformerModelArgs
 from .model.model import Transformer
 
 __all__ = [
-    "parallelize_llama",
-    "pipeline_llama",
+    "parallelize_qwen3",
+    "pipeline_qwen3",
     "TransformerModelArgs",
     "Transformer",
-    "llama3_configs",
+    "qwen3_configs",
 ]
 
 
-llama3_configs = {
+qwen3_configs = {
     "debugmodel": TransformerModelArgs(
-        dim=256, n_layers=6, n_heads=16, rope_theta=500000
+        dim=256, n_layers=6, n_heads=16, qk_norm=True, rope_theta=500000
     ),
     "debugmodel_flex_attn": TransformerModelArgs(
         dim=256,
         n_layers=6,
         n_heads=16,
+        qk_norm=True,
         rope_theta=500000,
         use_flex_attn=True,
         attn_mask_type="block_causal",
     ),
+    "4B": TransformerModelArgs(
+        dim=2560,
+        n_layers=36,
+        n_heads=32,
+        n_kv_heads=8,
+        qk_norm=True,
+        rope_theta=1000000,
+        head_dim=128,
+        norm_eps=1e-06,
+        vocab_size=151936,
+        ffn_dim_multiplier=1.425,  # To get intermediate_size=17408
+        multiple_of=128,
+        use_flex_attn=True,
+        attn_mask_type="block_causal_by_sequence_lengths",
+    ),
     "8B": TransformerModelArgs(
         dim=4096,
-        n_layers=32,
+        n_layers=36,
         n_heads=32,
         n_kv_heads=8,
-        ffn_dim_multiplier=1.3,
-        multiple_of=1024,
-        rope_theta=500000,
-    ),
-    "8B_scaled": TransformerModelArgs(
-        dim=4096,
-        n_layers=32,
-        n_heads=32,
-        n_kv_heads=8,
-        ffn_dim_multiplier=1.3,
-        multiple_of=1024,
-        rope_theta=500000,
-        rope_scaling=True,
-    ),
-    "8B_flex_attn": TransformerModelArgs(
-        dim=4096,
-        n_layers=32,
-        n_heads=32,
-        n_kv_heads=8,
-        ffn_dim_multiplier=1.3,
-        multiple_of=1024,
-        rope_theta=500000,
+        qk_norm=True,
+        rope_theta=1000000,
+        norm_eps=1e-06,
+        vocab_size=151936,
+        ffn_dim_multiplier=1.125,  # To get intermediate_size=12288
+        multiple_of=256,
         use_flex_attn=True,
         attn_mask_type="block_causal_by_sequence_lengths",
     ),
-    "8B_flex_attn_scaled": TransformerModelArgs(
-        dim=4096,
-        n_layers=32,
-        n_heads=32,
+    "14B": TransformerModelArgs(
+        dim=5120,
+        n_layers=40,
+        n_heads=40,
         n_kv_heads=8,
-        ffn_dim_multiplier=1.3,
-        multiple_of=1024,
-        rope_theta=500000,
-        rope_scaling=True,
+        qk_norm=True,
+        rope_theta=1000000,
+        norm_eps=1e-06,
+        vocab_size=151936,
+        ffn_dim_multiplier=1.275,  # To get intermediate_size=17408
+        multiple_of=256,
         use_flex_attn=True,
         attn_mask_type="block_causal_by_sequence_lengths",
-    ),
-    "70B": TransformerModelArgs(
-        dim=8192,
-        n_layers=80,
-        n_heads=64,
-        n_kv_heads=8,
-        ffn_dim_multiplier=1.3,
-        multiple_of=4096,
-        rope_theta=500000,
-    ),
-    "405B": TransformerModelArgs(
-        dim=16384,
-        n_layers=126,
-        n_heads=128,
-        n_kv_heads=8,
-        ffn_dim_multiplier=1.2,
-        multiple_of=4096,
-        rope_theta=500000,
     ),
 }
 
 register_train_spec(
     TrainSpec(
-        name="llama3",
+        name="qwen3",
         model_cls=Transformer,
-        model_args=llama3_configs,
-        parallelize_fn=parallelize_llama,
-        pipelining_fn=pipeline_llama,
+        model_args=qwen3_configs,
+        parallelize_fn=parallelize_qwen3,
+        pipelining_fn=pipeline_qwen3,
         build_optimizers_fn=build_optimizers,
         build_lr_schedulers_fn=build_lr_schedulers,
         build_dataloader_fn=build_dataloader,
--- torchtitan/models/qwen3/model/model.py
+++ torchtitan/models/qwen3/model/model.py
@@ -24,7 +24,7 @@ def apply_scaling(freqs: torch.Tensor) -> torch.Tensor:
     scale_factor = 8
     low_freq_factor = 1
     high_freq_factor = 4
-    old_context_len = 8192  # original llama3 length
+    old_context_len = 8192  # original Qwen3 length
 
     low_freq_wavelen = old_context_len / low_freq_factor
     high_freq_wavelen = old_context_len / high_freq_factor
@@ -176,7 +176,11 @@ class Attention(nn.Module):
             else model_args.n_kv_heads
         )
         self.n_rep = self.n_heads // self.n_kv_heads
-        self.head_dim = model_args.dim // model_args.n_heads
+        self.head_dim = (
+            model_args.dim // model_args.n_heads
+            if model_args.head_dim == -1
+            else model_args.head_dim
+        )
 
         self.wq = nn.Linear(
             model_args.dim, model_args.n_heads * self.head_dim, bias=False
@@ -186,6 +190,10 @@ class Attention(nn.Module):
         self.wo = nn.Linear(
             model_args.n_heads * self.head_dim, model_args.dim, bias=False
         )
+        self.qk_norm = model_args.qk_norm
+        if self.qk_norm:
+            self.q_norm = nn.RMSNorm(self.head_dim, eps=model_args.norm_eps)
+            self.k_norm = nn.RMSNorm(self.head_dim, eps=model_args.norm_eps)
         self.sdpa = build_attention(model_args.use_flex_attn, model_args.attn_mask_type)
 
     def init_weights(self, init_std: float):
@@ -221,6 +229,10 @@ class Attention(nn.Module):
         xq = xq.view(bs, seqlen, -1, self.head_dim)
         xk = xk.view(bs, seqlen, -1, self.head_dim)
         xv = xv.view(bs, seqlen, -1, self.head_dim)
+
+        if self.qk_norm:
+            xq = self.q_norm(xq)
+            xk = self.k_norm(xk)
 
         xq, xk = apply_rotary_emb(
             xq, xk, freqs_cis=freqs_cis, position_ids=position_ids
